c21f07f5d7133507e7e891ce05b0f60f8bf75f57
Antshel23 <antsheldon23@icloud.com>
2024-11-18 13:22:55 +0000

    calculateMonthlyRepayment function created

--- demo/src/main/java/projectapp/Services/MortgageService.java
+++ demo/src/main/java/projectapp/Services/MortgageService.java
@@ -5,5 +5,14 @@ import projectapp.Models.MortgageModel;
 
 @Service
 public class MortgageService {
-    
+    public double calculateMonthlyRepayment(MortgageModel mortgageModel) {
+        if ((mortgageModel.getInterestRate() <= 0) || (mortgageModel.getTermYears() <= 0) || (mortgageModel.getPropertyValue() <= 0) || (mortgageModel.getCurrentEquity() <= 0)) {
+            throw new IllegalArgumentException("Invalid data submitted");
+        }
+        double principal = mortgageModel.getPropertyValue() - mortgageModel.getCurrentEquity();
+        double interestRate = mortgageModel.getInterestRate() / 100 / 12;
+        double termMonths = mortgageModel.getTermYears() * 12;
+        double monthlyRepayment = (principal * interestRate) / (1 - Math.pow(1 + interestRate, -termMonths));
+        return monthlyRepayment;
+    }
 }
