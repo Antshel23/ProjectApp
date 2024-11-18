b58d9e1afe07d8c904b56853831243496e2b658f
Antshel23 <antsheldon23@icloud.com>
2024-11-18 13:40:04 +0000

    controller working for both pos + neg tests

--- demo/src/main/java/projectapp/Controllers/MortgageController.java
+++ demo/src/main/java/projectapp/Controllers/MortgageController.java
@@ -2,10 +2,8 @@ package projectapp.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import projectapp.Models.MortgageModel;
-import projectapp.Services.MortgageService;
 import projectapp.Services.MortgageService;
 
 
