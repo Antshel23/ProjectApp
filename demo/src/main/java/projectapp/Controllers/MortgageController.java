585b4218f36a75d8d4c025910634e809bae2d44c
Antshel23 <antsheldon23@icloud.com>
2024-11-18 11:39:38 +0000

    controller setup for post request and verified for pos/neg entries

--- demo/src/main/java/projectapp/Controllers/MortgageController.java
+++ demo/src/main/java/projectapp/Controllers/MortgageController.java
@@ -1,15 +1,21 @@
 package projectapp.Controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import projectapp.Models.MortgageModel;
-import projectapp.Services.MortgageService;
+// import projectapp.Services.MortgageService;
 
 
 @RestController
 @RequestMapping("/mortgage")
 public class MortgageController {
     
-    @Autowired
-    private MortgageService mortgageService;
+   // @Autowired
+    // private MortgageService mortgageService;
+
+    @PostMapping("/recieve")
+    public ResponseEntity<MortgageModel> recieveMortgageParams(@RequestBody MortgageModel mortgageModel) {
+        return ResponseEntity.ok(mortgageModel);
+    }
 }
