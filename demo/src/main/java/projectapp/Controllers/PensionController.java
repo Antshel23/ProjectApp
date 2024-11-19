e45dc8271420751ef1729b0d6cfbdd5876d5780c
Antshel23 <antsheldon23@icloud.com>
2024-11-19 15:15:59 +0000

    changed controller to use hashmap and correct return type

--- demo/src/main/java/projectapp/Controllers/PensionController.java
+++ demo/src/main/java/projectapp/Controllers/PensionController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.*;
 
 import projectapp.Models.PensionModel;
 import projectapp.Services.PensionService;
+import java.util.Map;
+import java.util.HashMap;
+import java.text.DecimalFormat;
 
 
 @RestController
@@ -14,9 +17,12 @@ public class PensionController {
     private PensionService pensionService;
 
     @PostMapping("/calculate/pot")
-    public ResponseEntity<PensionModel> returnX(@RequestBody PensionModel pensionModel) {
+    public ResponseEntity<Map<String, Object>> returnTotalPot(@RequestBody PensionModel pensionModel) {
         try {
-        return ResponseEntity.ok(pensionModel);
+            DecimalFormat df = new DecimalFormat("#.##");
+            Map<String, Object> response = new HashMap<>();
+            response.put("Total Pot", Double.valueOf(df.format(pensionService.calculateRetirementPot(pensionModel))));
+        return ResponseEntity.ok(response);
         }
         catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().body(null);
